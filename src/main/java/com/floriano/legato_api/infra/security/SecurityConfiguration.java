ee7a78b0dd60ac1534ced2db43bbb067d16b14dc
Erne1917 <ernestofamorim@gmail.com>
2025-11-24 22:12:36 -0300

    fix: cors config

--- src/main/java/com/floriano/legato_api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/floriano/legato_api/infra/security/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .cors(cors -> cors.disable())
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
